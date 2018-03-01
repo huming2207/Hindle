fb0a70f620242ff022e85174d0c2da8b10e18a4f
Jackson Ming Hu <huming2207@gmail.com>
2018-03-01 15:06:57 +1100

    Ptv: implement more functions

--- src/Network/Ptv.cpp
+++ src/Network/Ptv.cpp
@@ -2,4 +2,123 @@
 // Created by Ming Hu on 22/2/18.
 //
 
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <Configs/Defaults.h>
+#include <Configs/APIs.h>
 #include "Ptv.h"
+#include "SntpTime.h"
+
+transport_t Ptv::fetchTransportInfo()
+{
+  HTTPClient httpClient;
+
+  // Prepare query path
+  String queryPath = String("/v3/departures/route_type/")
+                        + HINDLE_TEMP_PTV_ROUTE_TYPE //FIXME: specify parameters by NVRAM
+                        + "/stop/"
+                        + HINDLE_TEMP_PTV_STOP_ID    //FIXME: specify parameters by NVRAM
+                        + "?max_results=1"           // One result only (to save RAM)
+                        + "&devid="
+                        + HINDLE_API_PTV_ID;
+
+  // Grab the signature out, thanks.
+  String signature = calculateSignature(queryPath, HINDLE_API_PTV_KEY);
+
+  // Now it should be "/v3/departures/route_type/0/stop/11101?devid=100100"
+  String fullUrl = queryPath + "&signature=" + signature;
+  log_i("Full URL: %s", fullUrl.c_str());
+
+  // Finally, fire the HTTP client and JSON parser
+  int httpStatus = httpClient.begin(fullUrl);
+
+  // Detect error
+  if(httpStatus < 0) {
+    log_e("Error: failed to access PTV API, return code %d!", httpStatus);
+    httpClient.end();
+    transport_t transportInfo = { .hasDisruption = false, .isSuccess = false };
+    return transportInfo;
+  }
+
+  // Grab the string and parse json
+  log_i("Finished downloading JSON data, prepare to parse...");
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& jsonObject = jsonBuffer.parseObject(httpClient.getStream());
+
+  // Detect JSON parsing error
+  if(!jsonObject.success()) {
+    log_e("Error: JSON data corrupted.");
+    httpClient.end();
+    jsonBuffer.clear();
+    transport_t transportInfo = { .hasDisruption = false, .isSuccess = false };
+    return transportInfo;
+  }
+
+  // Sometimes estimated departure string can be null, so use scheduled (timetabled) time instead
+  String firstTimeStr = jsonObject["departures"][0]["estimated_departure_utc"].as<String>();
+  String secondTimeStr = jsonObject["departures"][1]["estimated_departure_utc"].as<String>();
+
+  if(!firstTimeStr || firstTimeStr.length() < 1) {
+    firstTimeStr = jsonObject["departures"][0]["scheduled_departure_utc"].as<String>();
+  }
+
+  if(!secondTimeStr || secondTimeStr.length() < 1) {
+    secondTimeStr = jsonObject["departures"][1]["scheduled_departure_utc"].as<String>();
+  }
+
+
+  transport_t transportInfo = {
+      .hasDisruption = (jsonObject["disruptions"].as<JsonArray&>().size() > 0),
+      .isSuccess = (jsonObject["status"]["health"].as<int>() == 1),
+      .firstTime = SntpTime::parseIsoTimeStrToTm(firstTimeStr),
+      .secondTime = SntpTime::parseIsoTimeStrToTm(secondTimeStr),
+      .firstPlatform = (uint8_t)jsonObject["departures"][0]["platform_number"].as<unsigned short>(),
+      .secondPlatform = (uint8_t)jsonObject["departures"][1]["platform_number"].as<unsigned short>(),
+  };
+
+  httpClient.end();
+  jsonBuffer.clear();
+  return transportInfo;
+}
+
+/**
+ * Calculate PTV API's signature
+ * According to PTV API references
+ *  "The signature value is a HMAC-SHA1 hash of the completed request
+ *  (minus the base URL but including your user ID, known as “devid”)"
+ *
+ * ...so the query path should include API Developer ID, e.g. "/v3/departures/route_type/0/stop/11101?devid=100100"
+ *
+ * @param queryPath Mentioned above
+ * @param apiKey Your API Developer Key
+ * @return Signature, a 40-digit uppercase hex string.
+ */
+String Ptv::calculateSignature(String queryPath, const char * apiKey)
+{
+  log_i("Query URL path: %s", queryPath.c_str());
+  unsigned char hashResult[20];
+
+  // Calculate HMAC-SHA1 hash with mbedtls
+  int ret = mbedtls_md_hmac(mbedtls_md_info_from_type(MBEDTLS_MD_SHA1),   // Type: HMAC-SHA1
+                            (const uint8_t *)apiKey,                      // Key: API Key
+                            sizeof(apiKey),                               // Key size
+                            (const unsigned char*)queryPath.c_str(),      // Data: PTV API query URL payload
+                            sizeof(queryPath.c_str()),                    // Data size
+                            hashResult);                                  // Result to hashResult
+
+  if(ret != 0) {
+    log_e("Signature calculation failed, mbedtls returns a non-zero value: %d", ret);
+    return String();
+  }
+
+  // Convert hash buffer to PTV API ref specific signature string
+  String result;
+  for (unsigned char hashBit : hashResult) {
+    result += String(hashBit, HEX);
+  }
+
+  // ...be aware that they are case sensitive
+  result.toUpperCase();
+  log_i("Query signature: %s", result.c_str());
+  return result;
+}
--- src/Network/Ptv.h
+++ src/Network/Ptv.h
@@ -5,9 +5,32 @@
 #ifndef HINDLE_PTV_H
 #define HINDLE_PTV_H
 
+#include <ctime>
+#include <cstdint>
+#include <WString.h>
+
+/**
+ * PTV API response
+ *
+ * Only parse estimated departure to save hardware resources
+ * PTV punctuality normally sucks...
+ */
+typedef struct TransportInfo
+{
+  bool isSuccess;
+  bool hasDisruption;
+  uint8_t firstPlatform;
+  uint8_t secondPlatform;
+  struct std::tm firstTime;
+  struct std::tm secondTime;
+} transport_t;
+
 class Ptv
 {
-
+ public:
+  static transport_t fetchTransportInfo();
+ private:
+  static String calculateSignature(String queryPath, const char * apiKey);
 };
 
 #endif //HINDLE_PTV_H
